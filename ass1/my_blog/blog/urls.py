a62212e82e6353adaeca0d43d0bbfdd0ad36d225
aknur <95418973+aknurkappar@users.noreply.github.com>
2024-09-08 12:39:50 +0500

    Exercise 5: Creating Blog Views

--- ass1/my_blog/blog/urls.py
+++ ass1/my_blog/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hello),
+    path("hello/", views.hello),
+    path("posts/", views.get_posts),
+    path("posts/<int:id>/", views.get_post_by_id),
 ]
--- ass1/my_blog/blog/views.py
+++ ass1/my_blog/blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializer import PostSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
 def hello(request):
     return Response('Hello World!')
+
+@api_view(['GET'])
+def get_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_post_by_id(request, id):
+    print(request)
+    try:
+        post = Post.objects.get(pk=id)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PostSerializer(post)
+    return Response(serializer.data, status=status.HTTP_200_OK)
